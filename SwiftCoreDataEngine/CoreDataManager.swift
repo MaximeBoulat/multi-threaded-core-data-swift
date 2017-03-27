0336381bc2e35aca52a9b98444dd0a59f196ec6a
Maxime Boulat <maxime.boulat@gmail.com>
2017-03-27 14:39:35 -0700

    Implemented NSPersistentCoordinator.

--- SwiftCoreDataEngine/CoreDataManager.swift
+++ SwiftCoreDataEngine/CoreDataManager.swift
@@ -38,7 +38,7 @@ class CoreDataManager{
 			}
 		}
     }
-    
+	
     class CoreDataOperation: BlockOperation {
         var type: TransactionType = .none
         var identifier: String = ""
@@ -48,19 +48,38 @@ class CoreDataManager{
     
     init() {
         print("CoreDataManager initializing")
-        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
-        self.mainThreadContext = managedObjectContext
+//        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+//        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+        self.mainThreadContext = self.persistentContainer.viewContext
+		self.mainThreadContext?.automaticallyMergesChangesFromParent = true
+		 
+
     }
-    
+	
 	// MARK: Public
 	
 	func coordinateReading(identifier: String, block: @escaping (NSManagedObjectContext) -> Void){
-		serializeTransaction(type: .read, identifier: identifier, block: block)
+		
+		self.persistentContainer.performBackgroundTask { (context) in
+			
+			block(context)
+			try! context.save()
+			
+		}
+		
+//		serializeTransaction(type: .read, identifier: identifier, block: block)
 	}
 	
 	func coordinateWriting(identifier: String, block: @escaping (NSManagedObjectContext) -> Void){
-		serializeTransaction(type: .write, identifier: identifier, block: block)
+		
+		self.persistentContainer.performBackgroundTask { (context) in
+			
+			block(context)
+			try! context.save()
+			
+		}
+		
+//		serializeTransaction(type: .write, identifier: identifier, block: block)
 	}
 	
     func stressTest() {
@@ -105,7 +124,7 @@ class CoreDataManager{
                 }
             })
         }
-        
+		
         while self.stressing {
             switch OperationType(rawValue: Int(arc4random_uniform(3)))!{
             case .write:
@@ -117,10 +136,9 @@ class CoreDataManager{
             case .delete:
                 deleteBlock()
             }
-            
-            if coreDataQueue.operationCount > coreDataQueue.maxConcurrentOperationCount {
-                Thread.sleep(forTimeInterval: 0.1)
-            }
+			
+			 Thread.sleep(forTimeInterval: 0.007)
+			
         }
         self.coreDataQueue.cancelAllOperations()
     }
@@ -239,6 +257,53 @@ class CoreDataManager{
         }
         return coordinator
     }()
+	
+	
+	
+	// MARK: - Core Data stack
+	
+	lazy var persistentContainer: NSPersistentContainer = {
+		/*
+		The persistent container for the application. This implementation
+		creates and returns a container, having loaded the store for the
+		application to it. This property is optional since there are legitimate
+		error conditions that could cause the creation of the store to fail.
+		*/
+		let container = NSPersistentContainer(name: "Model")
+		container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+			if let error = error as NSError? {
+				// Replace this implementation with code to handle the error appropriately.
+				// fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+				
+				/*
+				Typical reasons for an error here include:
+				* The parent directory does not exist, cannot be created, or disallows writing.
+				* The persistent store is not accessible, due to permissions or data protection when the device is locked.
+				* The device is out of space.
+				* The store could not be migrated to the current model version.
+				Check the error message to determine what the actual problem was.
+				*/
+				fatalError("Unresolved error \(error), \(error.userInfo)")
+			}
+		})
+		return container
+	}()
+	
+	// MARK: - Core Data Saving support
+	
+	func saveContext () {
+		let context = persistentContainer.viewContext
+		if context.hasChanges {
+			do {
+				try context.save()
+			} catch {
+				// Replace this implementation with code to handle the error appropriately.
+				// fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+				let nserror = error as NSError
+				fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+			}
+		}
+	}
 }
 
 
